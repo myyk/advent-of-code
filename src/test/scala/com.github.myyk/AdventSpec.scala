45d4fccfb9132da7c6f6e57cd8bf929170b51b31
Myyk Seok <myyk.seok@gmail.com>
2018-12-08 15:04:30 +0800

    make into advent of code project

--- src/test/scala/com.github.myyk/AdventSpec.scala
+++ src/test/scala/com.github.myyk/AdventSpec.scala
@@ -2,7 +2,7 @@ package example
 
 import org.scalatest._
 
-class HelloSpec extends FlatSpec with Matchers {
+class AdventSpec extends FlatSpec with Matchers {
   "The Hello object" should "say hello" in {
     Hello.greeting shouldEqual "hello"
   }
